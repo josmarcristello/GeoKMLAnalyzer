dc516c4c4d2aaa19b48ba10676f3a03526587549
JBC <josmarbcristello@gmail.com>
2023-03-27 15:16:32 -0600

    Implementation of the whole process

---
+++ src/kmz_to_csv.py
@@ -0,0 +1,17 @@
+import os
+
+from functions import *
+
+## User changeable area ##
+filename_kml = 'Wetaskiwin_to_Leduc.kml' # File should be in [kml] folder.
+num_samples = 10                         # This is the number of samples to take along the path.
+## End of user changeable area ##
+
+load_dotenv()
+API_KEY = os.getenv('API_KEY') # Note: This is a Google API key. It should be stored in a .env file in the root directory.
+
+geo_df = kml2df(filename_kml)
+geo_df = get_elevation_data(geo_df, API_KEY, num_samples=num_samples)
+geo_df = calculate_distances(geo_df)
+write_to_csv(geo_df, filename_kml)
+plot_elevation_profile(geo_df, filename_kml, show_plot=False)
